c01a9a118bd9d48dcf91925ee1ff1947ecae87c8
Christian Saavedra <chrissaavedra2002@outlook.es>
2024-12-20 09:10:27 -0500

    refactor: Improve server startup with error handling and encapsulation
    - Encapsulated server startup logic in a startServer function.
    - Added try-catch block to handle errors during database connection.
    - Ensured the server exits with an error code if the database connection fails.

--- src/db.js
+++ src/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try{
         await mongoose.connect("mongodb+srv://201711202:o91996@clusterurp.k1azl.mongodb.net/");
-        console.log("Database connected");
+        console.log("Conexión con la base de datos exitosamente");
     }catch(error){
-        console.log(error);
+        console.log("Error al conectar con la base de datos", error);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import app from './app.js';
 import { connectDB } from './db.js';
 
+const PORT = 4000;
 
-connectDB();
-app.listen(4000)
-console.log('Server jordan on port', 4000)
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Servidor conectado en el puerto`, PORT);
+        });
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
