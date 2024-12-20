8770081e379602ef686634b086c5e18dd6247657
Christian Saavedra <chrissaavedra2002@outlook.es>
2024-12-20 09:46:21 -0500

    style: White design on RegistroPage, and correct form positions

--- cliente/src/pages/RegistroPage.jsx
+++ cliente/src/pages/RegistroPage.jsx
@@ -18,11 +18,11 @@ function RegistroPage() {
     });
 
     return (
-        <div className="flex h-[calc(100vh-100px)] items-center justify-center">
-            <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+        <div className="flex pt-44 items-center justify-center">
+            <div className="bg-white max-w-md p-10 rounded-md">
                 {
                     registroErrors.map((error, i) => (
-                        <div key={i} className="bg-red-500 p-2 text-white">
+                        <div key={i} className="bg-red-500 p-2 text-black">
                             {error}
                         </div>
                     ))
@@ -30,7 +30,7 @@ function RegistroPage() {
                 <form onSubmit={onSubmit}>
                     <h1 className="text-3xl font-bold my-2">Registro</h1>
                     <input type="text" {...register("username", { required: true })}
-                        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                        className="w-full bg-zinc-100 text-black px-4 py-2 rounded-md my-2"
                         placeholder="Nombre de usuario"
                     />
                     {
@@ -38,7 +38,7 @@ function RegistroPage() {
                             <p className="text-red-500">El usuario es requrido</p>
                         )}
                     <input type="email" {...register("email", { required: true })}
-                        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                        className="w-full bg-zinc-100 text-black px-4 py-2 rounded-md my-2"
                         placeholder="Correo electrónico"
                     />
                     {
@@ -46,18 +46,18 @@ function RegistroPage() {
                             <p className="text-red-500">El email es requrido</p>
                         )}
                     <input type="password" {...register("password", { required: true })}
-                        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                        className="w-full bg-zinc-100 text-black px-4 py-2 rounded-md my-2"
                         placeholder="Contraseña"
                     />
                     {
                         errors.password && (
                             <p className="text-red-500">La contraseña es requrida</p>
                         )}
-                    <button className="bg-sky-500 text-white px-4 py-2 rounded-md my-2" type="sumit">Registrar</button>
+                    <button className="bg-emerald-700 hover:bg-emerald-800 text-white px-4 py-2 rounded-md my-2" type="sumit">Registrar</button>
                 </form>
 
                 <p className="flex gap-x-2 justify-between">
-                Ya registrado? <Link to="/Login" className="text-sky-500">Iniciar Sesion</Link>
+                Ya registrado? <Link to="/Login" className="text-emerald-700">Iniciar Sesion</Link>
             </p>
 
             </div>
